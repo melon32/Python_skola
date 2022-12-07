f64983a02275c4d2d8ffae4bf28ae95840119993
Andris Andersons <andersonsandris890@gmail.com>
2022-12-07 10:42:50 +0200

    stunda

--- append.py
+++ append.py
@@ -1,4 +1,5 @@
 
+
 # Define the list of numbers
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
--- encode.py
+++ encode.py
@@ -1,3 +1,11 @@
-x = 'Estere'
+# unicode string
+string = 'pythön!'
 
-print(x.encode(encoding = 'ascii', errors = "ignore"))
+# print string
+print('The string is:', string)
+
+# default encoding to utf-8
+string_utf = string.encode()
+
+# print result
+print('The encoded version is:', string_utf)
